896c5d612de01f6e3c3193e3fce2d37a0c6b56dd
Hibai Unzueta <hibai.unzueta@gmail.com>
2023-03-06 18:13:49 +0100

    Update 2

--- src/PATTERNS.js
+++ src/PATTERNS.js
@@ -22,7 +22,7 @@ const table = [
     problem:
       "If the population of a region is weighted too far toward small villages, modern civilization can never emerge; but if the population is weighted too far toward big cities, the earth will go to ruin because the population isn't where it needs to be, to take care of it.",
     therefore:
-      "Encourage a birth and death process for towns within the region, which gradually has these effects:  1. The population is evenly distributed in terms of different sizes- example, one town with 1,000,000 people, 10 towns with 100,000 people each, 100 towns with 10,000 people each, and 1000 towns with 1000 people each.  2. These towns are distributed in space in such a way that within each size category the towns are homogeneously distributed all across the region.",
+      "Encourage a birth and death process for towns within the region, which gradually has these effects:  1. The population is evenly distributed in terms of different sizes- example, one town with 1,000,000 people, 10 towns with 100,000 people each, 100 towns with 10,000 people each, and 1000 towns with 1000 people each.  2. These towns are distributed in space in such a way that within each size category the towns are homogeneously distributed all across the region. This progress can be implemented by regional zoning policies, land grants, and incentives which encourage industries to locate according to the dictates of the distribution.",
     bookPage: 16,
   },
   {
